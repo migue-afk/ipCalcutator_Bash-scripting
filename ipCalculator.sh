386f172548ef46934dbf7bbdb946511d83cd953a
migue-afk <miguef10hd@gmail.com>
2024-03-01 23:02:33 -0500

    add conversion ip address and network mask

--- ipCalculator.sh
+++ ipCalculator.sh
@@ -23,40 +23,65 @@ function helpPanel(){
 
 
 trap ctrl_c INT
-function ipToBinary(){
-	ipDir="$ipDir"
-	echo -e "$ipDir" > reportIP.txt
+function decToBinary(){
+	decimal="$decimal"
+	echo -e "$decimal" > report.txt
 	for num in $(seq 1 4) ; do
-		ipBin=$(cat reportIP.txt | awk -v n="$num" -F. '{print $n}')
-		echo "obase=2;$ipBin" | bc
+		decBin=$(cat report.txt | awk -v n="$num" -F. '{print $n}')
+		decBins=$(echo "obase=2;$decBin" | bc);
+		str="$str$decBins."
 	done
+	str=${str%?}
+	echo "ip Binary $str"
+
 }
 
-function maskToBinary(){
+
+
+function networkId(){
+	ipDir="$ipDir"
+		echo -e "$ipDir" > report.txt
+		for num in $(seq 1 4) ; do
+			ipBin=$(cat report.txt | awk -v n="$num" -F. '{print $n}')
+			ipBins=$(echo "obase=2;$ipBin" | bc);
+			str="$str$ipBins."
+		done
+		str=${str%?}
+		echo "ip Binary   $str --> $(cat report.txt)"
+
 	mask="$mask"
-	echo -e "$mask" > reportIP.txt
-	for num in $(seq 1 4) ; do
-		maskBin=$(cat reportIP.txt | awk -v n="$num" -F. '{print $n}')
-		echo "obase=2;$maskBin" | bc
-	done
+		echo -e "$mask" > report.txt
+		for num in $(seq 1 4) ; do
+			ipMask=$(cat report.txt | awk -v n="$num" -F. '{print $n}')
+			ipMasks=$(echo "obase=2;$ipMask" | bc);
+			strm="$strm$ipMasks."
+		done
+		strm=${strm%?}
+		echo "mask Binary $strm -- $(cat report.txt)"
 }
 #-----------------------------------------------
 #Variables
 #-----------------------------------------------
 
 declare -i parameterCount=0
+declare -i secCountIp=0
+declare -i secCountMs=0
+str=""
+strm=""
 
 while getopts "i:m:" arg ;do
 	case $arg in
-	i) ipDir=$OPTARG;  let parameterCount+=1;;
-	m) mask=$OPTARG; let parameterCount+=2;;
+	i) decimal=$OPTARG; secCountIp=1;let parameterCount+=1;;
+	m) mask=$OPTARG; secCountMs=1;let parameterCount+=2;;
 esac
 done
 
 if [ $parameterCount -eq 1 ]; then
-	ipToBinary $ipDir
+	decToBinary $decimal
 elif [ $parameterCount -eq 2 ];then
 	maskToBinary $mask
+elif [ $secCountIp -eq 1 ] && [ $secCountMs -eq 1 ]; then
+	networkId $decimal $mask
 else
 	helpPanel
 fi
